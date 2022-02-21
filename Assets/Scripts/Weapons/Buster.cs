b007caf0f3893e4f83aac084dc39d5dd434ad74e
João Neto <joao.neto.feup@gmail.com>
2022-02-21 22:30:14 +0000

    Shooting Stars

--- Assets/Scripts/Weapons/Buster.cs
+++ Assets/Scripts/Weapons/Buster.cs
@@ -5,9 +5,12 @@ namespace Assets.Scripts.Weapons
 
     public class Buster : MonoBehaviour
     {
+        public GameObject ProjectileGameObject;
         [SerializeField] private InputActionReference _triggerAction;
 
         private BusterState _busterState;
+
+        private float _chargeTime;
 
         private enum BusterState
         {
@@ -23,21 +26,24 @@ namespace Assets.Scripts.Weapons
 
         private void TriggerCancelled(InputAction.CallbackContext obj)
         {
-            Debug.Log("Pew pew!");
             _busterState = BusterState.Idle;
+
+            var projectile = Instantiate(ProjectileGameObject, transform.position, Quaternion.identity);
+            projectile.GetComponent<Rigidbody>().velocity = transform.forward*_chargeTime;
         }
 
 
         private void TriggerStart(InputAction.CallbackContext obj)
         {
             _busterState = BusterState.Charging;
+            _chargeTime = 0;
         }
 
-        // Update is called once per frame
         private void Update()
         {
             if (_busterState == BusterState.Charging)
             {
+                _chargeTime += Time.deltaTime;
             }
         }
 
